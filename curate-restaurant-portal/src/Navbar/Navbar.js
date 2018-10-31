8a65a280214fd674ee7c0572d860015318c863d8
tuckerwilliams <tuckerwilliams1@gmail.com>
2018-10-31 11:25:37 -0400

    Moved conditional check outsideof return statement

--- curate-restaurant-portal/src/Navbar/Navbar.js
+++ curate-restaurant-portal/src/Navbar/Navbar.js
@@ -23,26 +23,31 @@ class Navbar extends Component {
   }
 
   render() {
+
+    var loginLogoutButton = null;
+
+    if (!this.state.loggedIn) {
+      loginLogoutButton = (
+        <button className="login" onClick={this.handleRestaurantLogin}>
+          <Link to="/myrestaurants">Login to my restaurant</Link>
+        </button>
+      );
+    } else {
+      loginLogoutButton = (
+        <button className="login" onClick={this.handleRestaurantLogout}>
+          <Link to="/">{this.state.user}: Log Out</Link>
+        </button>
+      );
+    }
   
     return (
       <Router>
         <div className="Navbar">
-            <img src={navbar_icon}/>
-            {!this.state.loggedIn && 
-              <button className="login" onClick={this.handleRestaurantLogin}>
-                <Link to="/myrestaurants">Login to my restaurant</Link>
-              </button>
-            }
-
-            { this.state.loggedIn &&
-                <button className="login" onClick={this.handleRestaurantLogout}>
-                  <Link to="/">{this.state.user}: Log Out</Link>
-                </button>
-            }
+          <img alt="" src={navbar_icon}/>
+          {loginLogoutButton}
 
           <Route path="/myrestaurants" component={RestaurantHomePage} />
           <Route exact={true} path="/" component={Main} />
-
         </div>
       </Router>
     );
